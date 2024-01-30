902c0e469eca498cf3b7ee8f3b34514d3a669f7a
Uzair05 <ubcasim@gmail.com>
2024-01-30 16:05:36 +0800

    Solved

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -6,17 +7,23 @@ struct Node {
     int M{3};
 };
 
-int heuristic(Node h) { return h.C + h.M; }
+auto heuristic = [](const Node& h) { return h.C + h.M; };
 
-void findSol(Node head, bool boatOnLeft = true) {
+std::vector<Node> findSol(Node head, bool boatOnLeft = true) {
+    std::vector<Node> solution{};
     std::vector<Node> possible_combinations{};
 
     if (boatOnLeft) {
-        possible_combinations.push_back({head.C - 1, head.M});
-        possible_combinations.push_back({head.C - 1, head.M - 1});
-        possible_combinations.push_back({head.C, head.M - 1});
-        possible_combinations.push_back({head.C - 2, head.M});
-        possible_combinations.push_back({head.C, head.M - 2});
+        for (int i{1}; i <= 2 && i <= head.C; i++) {
+            possible_combinations.push_back({head.C - i, head.M});
+        }
+        for (int i{1}; i <= 2 && i <= head.M; i++) {
+            possible_combinations.push_back({head.C, head.M - i});
+        }
+
+        if (head.C >= 1 && head.M >= 1) {
+            possible_combinations.push_back({head.C - 1, head.M - 1});
+        }
     } else {
         Node onRight = {3 - head.C, 3 - head.M};
         for (int i{1}; i <= 2 && i <= onRight.C; i++) {
@@ -26,25 +33,54 @@ void findSol(Node head, bool boatOnLeft = true) {
             possible_combinations.push_back({head.C, head.M + i});
         }
 
-        if (onRight.M > 1 and onRight.C > 1) {
+        if (onRight.M >= 1 && onRight.C >= 1) {
             possible_combinations.push_back({head.C + 1, head.M + 1});
         }
     }
 
-    for (const auto& i : possible_combinations) {
-        std::cout << "C " << i.C << " : M " << i.M << "\n";
+    std::erase_if(possible_combinations, [boatOnLeft](const auto& n) {
+        return (!boatOnLeft ? (((n.C - 1) > n.M) && (n.M > 0)) ||
+                                  ((3 - n.C) > (3 - n.M) && (n.M < 3))
+                            : ((n.C > n.M) && (n.M > 0)) ||
+                                  (((3 - n.C) - 1) > (3 - n.M) && (n.M < 3)));
+    });
+
+    if (possible_combinations.size() == 0) return std::vector<Node>{};
+
+    std::sort(possible_combinations.begin(), possible_combinations.end(),
+              [](const Node& a, const Node& b) {
+                  return heuristic(a) < heuristic(b);
+              });
+
+    if (auto it = std::find_if(possible_combinations.begin(),
+                               possible_combinations.end(),
+                               [](Node& n) { return (n.C + n.M) == 0; });
+        it != possible_combinations.end()) {
+        solution.push_back(*it);
+    } else {
+        for (const auto& pc : possible_combinations) {
+            auto s = findSol(pc, !boatOnLeft);
+            if (s.size() > 0) {
+                solution.push_back(pc);
+                for (const auto& s_ : s) {
+                    solution.push_back(s_);
+                }
+                break;
+            }
+        }
     }
+
+    return solution;
 }
 
 int main() {
-    Node start_situation{1, 1};
-    findSol(start_situation, false);
+    Node start_situation;
+    auto i = findSol(start_situation);
 
-    // std::vector<Node> sol{findSol(start_situation)};
-
-    // for(const auto& i: sol){
-    //     std::cout << "C " << i.C << " : M " << i.M <<"\n";
-    // }std::cout << std::endl;
+    for (const auto& c : i) {
+        std::cout << "C: " << c.C << ", M: " << c.M << "\t\t";
+        std::cout << "C: " << 3 - c.C << ", M: " << 3 - c.M << "\n";
+    }
 
     return 0;
 }
